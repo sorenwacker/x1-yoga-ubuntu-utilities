ac06b9e9590ceb4ac83743e9e5d2ec1529c6820c
swacker <swacker@local>
2025-11-16 18:53:56 +0100

    Fix decimal separator in brightness OSD level using LC_NUMERIC=C

--- scripts/brightness-down.sh
+++ scripts/brightness-down.sh
@@ -10,7 +10,7 @@ brightness=$(echo "$result" | awk -F'[(),]' '{print $2}' | tr -d ' ')
 
 # Show OSD slider (requires Custom OSD extension)
 if [ -n "$brightness" ]; then
-    level=$(awk "BEGIN {printf \"%.2f\", $brightness / 100}")
+    level=$(LC_NUMERIC=C awk "BEGIN {printf \"%.2f\", $brightness / 100}")
     GSETTINGS_SCHEMA_DIR=$HOME/.local/share/gnome-shell/extensions/custom-osd@neuromorph/schemas \
         gsettings set org.gnome.shell.extensions.custom-osd showosd "$RANDOM,display-brightness-symbolic,,${level}"
 fi
